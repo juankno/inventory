b39f6f50bf00bd8a5a219131d68e92e713d47618
Juankno <juanes0202@gmail.com>
2025-06-07 16:13:17 -0500

    feat: agregar restricción de unicidad al nombre del producto y establecer relación con categorías

--- database/migrations/2025_06_07_164258_create_products_table.php
+++ database/migrations/2025_06_07_164258_create_products_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
             $table->unsignedInteger('stock')->default(0);
+            $table->foreignId('category_id')->nullable()->constrained('categories');
             $table->timestamps();
         });
     }
